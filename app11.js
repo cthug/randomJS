ebaa2de0de36c4fa2f6d3e52a90a72e05c45251b
Randell Womack <randelljw92@gmail.com>
2021-03-22 12:48:49 -0700

    working with array basics

---
+++ app11.js
@@ -0,0 +1,44 @@
+//arrays & objects
+//let shoppinList = ['cereal', 'cheese', 'ice'];
+
+//let lotto = [45, 12, 23, 25, 34];
+
+//let myCollection = [12, 'dog', true, null, Nan,];
+//
+
+//let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+//let shoppingList = ['cheddar cheese', '2% milk'];
+//shoppingList[1] = 'whole Milk';
+//shoppingList[2] = 'Ice Cream';
+
+//shoppingList[shoppingList.length] = 'Tomatoes';
+//shoppingList[shoppingList.length] = 'potatoes';
+
+//let topSongs = [
+//    'First time Ever I Saw Your Face',
+//    'God Only Knows',
+//    'A Day In The Life',
+//    'Life On Mars'
+//];
+//topSongs.push('Fourtunate Son');
+//topSongs.push('gravity');
+
+//topSongs.pop() will remove the array list one at a time
+
+//shift will remove from beginning
+//unshift add to the start
+
+let topSongs = [
+    'First time Ever I Saw Your Face',
+    'God Only Knows',
+    'A Day In The Life',
+    'Life On Mars'
+];
+topSongs.push('Fourtunate Son');
+topSongs.push('gravity');
+//topSongs.length
+//topsongs.unshift('What Kind of Woman Is This')
+//topSongs.shift('What kind of Woman Is This')
+//topSongs.unshift('really', 'love me')
+//can add multiples with .push/.pop and .unshift and .shift
