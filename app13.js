a1139b0df4a41237b7bdcdcdbf5afecac0daccdd
Randell Womack <randelljw92@gmail.com>
2021-03-22 13:36:00 -0700

    using reverse and join with array basics

---
+++ app13.js
@@ -0,0 +1,9 @@
+//reverse and join with arrays
+let letters = ['t', 'c', 'e', 'p', 's', 'e', 'R'];
+
+letters.reverse()
+//letters.join()
+//letters.join('.')
+//letters.join('-')
+//[12,3,600,flase,true].join() will still work and convert into new strings
+
