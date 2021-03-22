1d081252d7dcb50fc6e11065812b2a8bf233ddaa
Randell Womack <randelljw92@gmail.com>
2021-03-22 16:14:51 -0700

    using .sort

--- app14.js
+++ app14.js
@@ -14,11 +14,23 @@ let animals = ['shark', 'salmon', 'whale',
 //let copy = animals.slice()
 
 //splice
-animals.splice(1, 0, 'octopus');
-animals.splice(3, 2)
-animals.splice(3,0,'Snake','frog')
-animals.splice(0, 2, 'Shark', 'Octopus')
+//animals.splice(1, 0, 'octopus');
+//animals.splice(3, 2)
+//animals.splice(3,0,'Snake','frog')
+//animals.splice(0, 2, 'Shark', 'Octopus')
 
 //.splice(startIndex,deleteCount,itemsToInsert);
 //above is how splice works and is the pattern that needs to be followed
 
+//sort method
+let people = [
+    'Mrs. Robinson', 'Angie', 'Jolene', 'Maggie may', 'Roxanne'
+];
+people.sort()
+
+let numbers = [
+    34, 10, 10000, 67, 99
+];
+numbers.sort()
+//compare function is needed to get result desired with numbers.
+//due to .sort converting everything to a string in this language specifically 
