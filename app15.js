cdcb9a809a1dfaa79b9f1f0608ea63b7cd706b2d
Randell Womack <randelljw92@gmail.com>
2021-03-22 16:36:20 -0700

    const with array syntax

---
+++ app15.js
@@ -0,0 +1,15 @@
+//using const with array
+
+const foods = ['milk']
+foods.push('chocolate')
+foods.unshift('tortillas')
+foods.pop()
+foods.pop()
+foods.pop()
+foods.push('lemon')
+foods.push('kale')
+foods.push('cheese')
+foods.push('milk')
+
+//easily change contents within array but due to const i can never change
+//the actual array reference,only the contents inside. 
