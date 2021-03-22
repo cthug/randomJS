9e7cc8cb302dc1419d5800fe0c0f4479c42775e2
Randell Womack <randelljw92@gmail.com>
2021-03-22 09:57:31 -0700

    another logical operator

---
+++ app7.js
@@ -0,0 +1,18 @@
+//final logical operator
+
+let loggedInUser;
+//if there is not a logged in user
+
+if (!loggedInUser) {
+    console.log('get out of here!');
+}
+
+let flavor = 'watermelon';
+
+//if (flavor !== 'grape' && flavor !== 'cherry') {
+//  console.log('we dont have that flavor!'); 
+//}
+
+if (!(flavor === 'grape' || flavor === 'cherry')) {
+    console.log('We only offer grape and cherry!');
+}
