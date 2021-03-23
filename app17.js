0e1624fe65269dcc8aba9e3b11b9f716346318d3
Randell Womack <randelljw92@gmail.com>
2021-03-23 10:07:26 -0700

    adding and updating properties

---
+++ app17.js
@@ -0,0 +1,8 @@
+//adding and updating properties
+//simple examples follow
+const userReviews = {};
+userReviews['queenBee49'] = 4.0;
+userReviews.mrSmith78 = 3.5;
+userReviews['colt'] = 5;
+userReviews['queenBee49'] += 2;
+userReviews.mrSmith78++;
