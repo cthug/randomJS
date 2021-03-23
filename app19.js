d8f1220edc1b84e259427e7ea23344f651e18e83
Randell Womack <randelljw92@gmail.com>
2021-03-23 13:12:17 -0700

    simple for loop examples

--- app19.js
+++ app19.js
@@ -5,6 +5,14 @@
 //    console.log('hello:' , i);
 //}
 
-for (let i = 1; i <= 10; i+= 3) {
-    console.log('hello:', i);
+//for (let i = 1; i <= 10; i+= 3) {
+//    console.log('hello:', i);
+//}
+
+//for (let num = 1; num <= 20; num++) {
+//    console.log(`${num}x${num} = ${num * num}`);
+//}
+
+for (let i = 200; i >= 0; i -= 25) {
+    console.log(i);
 }
