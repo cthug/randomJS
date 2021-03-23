44f3c06666eefe413ce6ef76d7d95fc2e297d37f
Randell Womack <randelljw92@gmail.com>
2021-03-22 17:08:16 -0700

    simple object literal example

--- app16.js
+++ app16.js
@@ -1,6 +1,31 @@
 //nested array
-const board = [
-    ['O',null,'X'],
-    [null, 'X', 'O'],
-    ['X','O',null],
-]
+//const board = [
+//    ['O',null,'X'],
+//    [null, 'X', 'O'],
+//    ['X','O',null],
+//]
+
+//object
+//objects are collections of properties
+//properties are a key value pair
+// rather than accessing index, we use custom keys
+
+//creating object literals
+
+const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep:    '2:13',
+};
+//fitBitData.totalMiles
+// 5755
+//fitBitData.avgCalorieBurn
+// 5755
+//the . 
+//allows you to reference information
+//specifically desired within the object literal
+
+//let data = { a: 1, b: 2, c: 3, }; another simple example
+
