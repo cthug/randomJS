976fb80ecf896081adf7ff518ffd13b424542e73
Randell Womack <randelljw92@gmail.com>
2021-03-23 11:24:03 -0700

    simple array of object structure examples

--- app17.js
+++ app17.js
@@ -1,8 +1,59 @@
 //adding and updating properties
 //simple examples follow
-const userReviews = {};
-userReviews['queenBee49'] = 4.0;
-userReviews.mrSmith78 = 3.5;
-userReviews['colt'] = 5;
-userReviews['queenBee49'] += 2;
-userReviews.mrSmith78++;
+//const userReviews = {};
+//userReviews['queenBee49'] = 4.0;
+//userReviews.mrSmith78 = 3.5;
+//userReviews['colt'] = 5;
+//userReviews['queenBee49'] += 2;
+//userReviews.mrSmith78++;
+
+//nested arrays and objects
+
+//const student = {
+//    firstName: 'David',
+//    lastName: 'jones',
+//    stregths: ['music', 'art',],
+//    exams: {
+//        midterm: 92,
+//        final: 88
+//    }
+//};
+
+//const avg = (student.exams.midterm + student.exams.final)
+//    / 2;
+
+//another ex. of an array of objects
+
+const shoppingCart = [
+    {
+        product: 'Jenga Classic',
+        price: 6.88,
+        quantity: 1
+    },
+    {
+        product: 'Echo Dot',
+        price: 29.99,
+        quantity: 3
+    },
+    {
+        product: 'Fire Stick',
+        price: 39.99,
+        quantity: 2
+    }
+];
+
+//another nesting example/structure in array of objects  
+const game = {
+    player1: {
+        username: 'blue',
+        playingAs: 'X'
+    },
+    player2: {
+        username: 'johnny',
+        playingAs: 'O'
+    },
+    board: [['O', null, 'X'], ['X', '0', 'X'], [null, 'O', 'X']],
+    
+};
+
+
