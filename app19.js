e41c70830e3447a04d3d949c06cc1e352b73a286
Randell Womack <randelljw92@gmail.com>
2021-03-23 12:48:40 -0700

    basic loop concept

---
+++ app19.js
@@ -0,0 +1,10 @@
+//js loops
+//for loop
+//initial value//when to run loop//how to change value
+//for (let i = 1; i <= 10; i++) {
+//    console.log('hello:' , i);
+//}
+
+for (let i = 1; i <= 10; i+= 3) {
+    console.log('hello:', i);
+}
