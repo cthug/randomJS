c9b79a2663c2c0df81286016e682efb07a4f1697
Randell Womack <randelljw92@gmail.com>
2021-03-22 17:24:33 -0700

    accessing literal objects

--- app16.js
+++ app16.js
@@ -12,13 +12,13 @@
 
 //creating object literals
 
-const fitBitData = {
-    totalSteps: 308727,
-    totalMiles: 211.7,
-    avgCalorieBurn: 5755,
-    workoutsThisWeek: '5 of 7',
-    avgGoodSleep:    '2:13',
-};
+//const fitBitData = {
+//   totalSteps: 308727,
+//   totalMiles: 211.7,
+//   avgCalorieBurn: 5755,
+//   workoutsThisWeek: '5 of 7',
+//   avgGoodSleep:    '2:13',
+//};
 //fitBitData.totalMiles
 // 5755
 //fitBitData.avgCalorieBurn
@@ -29,3 +29,20 @@ const fitBitData = {
 
 //let data = { a: 1, b: 2, c: 3, }; another simple example
 
+//accessing object literals
+const palette = {
+    red: '#eb4d4b',
+    yellow: '#f9ca24',
+    blue: '#30336b'
+}
+
+//palette.blue
+//palette.red
+//palette.yellow
+
+let mysteryColor = 'yellow';
+
+//palette.mysteryColor will read undefined
+palette[mysteryColor]
+//use square brackets to access variable or some dynamic value within
+//the object literal
