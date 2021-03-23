0ba3b0667e1ec12c9cff42086f52502046501b61
Randell Womack <randelljw92@gmail.com>
2021-03-23 14:35:39 -0700

    loops + arrays

--- app19.js
+++ app19.js
@@ -22,3 +22,37 @@
 //console.log(i);
 //}
 //always avoid infinite loops. 
+
+//loops + arrays
+
+const animals = ['lions', 'tigers', 'bears'];
+for (let i = 0; i < animals.length; i++){
+    console.log(i, animals[i]);
+}
+
+const examScores = [98, 77, 84, 91, 57, 66];
+for (let i = 0; i < examScores.length; i++){
+    console.log(i, examScores[i]);
+}
+const myStudents = [
+    {
+        firstName: 'Artemis',
+        grade: 86
+    },
+    {
+        firstName: 'Zues',
+        grade: 97
+    },
+    {
+        firstName: 'Hera',
+        grade: 72
+    },
+    {
+        firstName: 'Apollo',
+        grade: 90
+    },
+];
+for (let i = 0; i < myStudents.length; i++){
+    let student = myStudents[i];
+    console.log(`${student.firstName} scored ${student.grade}`);
+}
