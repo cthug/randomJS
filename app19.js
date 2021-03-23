bd476506a526520fbd1b216b5a08ad106e404e63
Randell Womack <randelljw92@gmail.com>
2021-03-23 14:36:08 -0700

    loops + arrays

--- app19.js
+++ app19.js
@@ -25,15 +25,15 @@
 
 //loops + arrays
 
-const animals = ['lions', 'tigers', 'bears'];
-for (let i = 0; i < animals.length; i++){
-    console.log(i, animals[i]);
-}
+//const animals = ['lions', 'tigers', 'bears'];
+//for (let i = 0; i < animals.length; i++){
+//    console.log(i, animals[i]);
+//}
 
-const examScores = [98, 77, 84, 91, 57, 66];
-for (let i = 0; i < examScores.length; i++){
-    console.log(i, examScores[i]);
-}
+//const examScores = [98, 77, 84, 91, 57, 66];
+//for (let i = 0; i < examScores.length; i++){
+//    console.log(i, examScores[i]);
+//}
 const myStudents = [
     {
         firstName: 'Artemis',
@@ -52,7 +52,25 @@ const myStudents = [
         grade: 90
     },
 ];
-for (let i = 0; i < myStudents.length; i++){
+//for (let i = 0; i < myStudents.length; i++) {
+//    let student = myStudents[i];
+//    console.log(`${student.firstName} scored ${student.grade}`);
+//}
+let total = 0;
+for (let i = 0; i < myStudents.length; i++) {
     let student = myStudents[i];
-    console.log(`${student.firstName} scored ${student.grade}`);
+    total += student.grade;
 }
+console.log(total/myStudents.length);
+//printing in reverse
+//const word = 'stressed';
+//for (let i = word.length - 1; i >= 0; i--){
+//    console.log(word[i]);
+//}
+
+//const word = 'stressed';
+//let reversedWord = '';
+//for (let i = word.length - 1; i >= 0; i--){
+//    reversedWord += word[i];
+//    console.log(reversedWord);
+//}
