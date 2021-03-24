35657383155397fe6773fe488b61d291c20fa15e
Randell Womack <randelljw92@gmail.com>
2021-03-24 13:48:59 -0700

    for of with objects

---
+++ app22.js
@@ -0,0 +1,24 @@
+//for of with objects
+
+const movieReviews = {
+    Arrival: 9.5,
+    Alien: 9,
+    Amelie: 8,
+    'In Bruges': 9,
+    Amedeus: 10,
+    'Kill Bill': 8,
+    'Little Miss Sunshine': 8.5,
+    Coraline: 7.5
+};
+
+for (let movie of Object.keys(movieReviews)) {
+    console.log(movie, movieReviews[movie]);
+}
+
+const ratings = Object.values(movieReviews);
+let total = 0
+for (let r of ratings) {
+    total += r;
+}
+let avg = total / ratings.length;
+console.log('average:',avg);
