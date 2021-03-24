001bbde86afaf4aea0242e700b786fbba2385574
Randell Womack <randelljw92@gmail.com>
2021-03-24 14:03:20 -0700

    for in loop example

--- app22.js
+++ app22.js
@@ -1,24 +1,40 @@
 //for of with objects
 
-const movieReviews = {
-    Arrival: 9.5,
-    Alien: 9,
-    Amelie: 8,
-    'In Bruges': 9,
-    Amedeus: 10,
-    'Kill Bill': 8,
-    'Little Miss Sunshine': 8.5,
-    Coraline: 7.5
+//const movieReviews = {
+//    Arrival: 9.5,
+//    Alien: 9,
+//    Amelie: 8,
+//    'In Bruges': 9,
+//    Amedeus: 10,
+//    'Kill Bill': 8,
+//    'Little Miss Sunshine': 8.5,
+//    Coraline: 7.5
+//};
+
+//for (let movie of Object.keys(movieReviews)) {
+//    console.log(movie, movieReviews[movie]);
+//}
+
+//const ratings = Object.values(movieReviews);
+//let total = 0
+//for (let r of ratings) {
+//    total += r;
+//}
+//let avg = total / ratings.length;
+//console.log('average:',avg);//
+
+//now the for(in) loop
+
+const jeopardyWinnigs = {
+    regularPlay: 2522700,
+    watsonChallenge: 300000,
+    tounamentOfChampions: 500000,
+    battleOfTheDecades: 100000,
+
 };
-
-for (let movie of Object.keys(movieReviews)) {
-    console.log(movie, movieReviews[movie]);
+let total = 0;
+for (let prop in jeopardyWinnigs) {
+    console.log(prop);
+    total += (jeopardyWinnigs[prop]);
 }
-
-const ratings = Object.values(movieReviews);
-let total = 0
-for (let r of ratings) {
-    total += r;
-}
-let avg = total / ratings.length;
-console.log('average:',avg);
+console.log(`ken Jennings Total Earnings: ${total}`);
