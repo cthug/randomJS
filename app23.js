e7186c42269b82df3d9cdf17e7b664e9b4f202ec
Randell Womack <randelljw92@gmail.com>
2021-03-28 21:51:08 -0700

    understanding return value

--- app23.js
+++ app23.js
@@ -19,16 +19,16 @@
 //    grumpus()
 //}
 
-function rollDie() {
-    let roll = Math.floor(Math.random() * 6) + 1;
-    console.log(`Rolled: ${roll}`);
-}
+//function rollDie() {
+//    let roll = Math.floor(Math.random() * 6) + 1;
+//    console.log(`Rolled: ${roll}`);
+//}
 
-function throwDice(numRolls) {
-    for (let i = 0; i < numRolls; i++){
-        rollDie();
-    }
-}
+//function throwDice(numRolls) {
+//    for (let i = 0; i < numRolls; i++){
+//        rollDie();
+//    }
+//}
 
 
 //function greet(nickName) {
@@ -36,3 +36,45 @@ function throwDice(numRolls) {
 //    console.log(`Hi, ${name}!`);
 //}
  
+//functons with multiple args
+
+//simple concept of function syntax
+
+//function square(num) {
+//    console.log(num * num);
+//}
+//function sum(x, y) {
+//    console.log(x + y);
+//}
+//function divide(a, b) {
+//    console.log(a / b);
+//}
+
+//const scream = 'hello'.toUpperCase();
+//console log is not the same as a value being returned 
+
+//understanding return value 
+//function add(x, y) {
+//    return x + y;
+//}
+//there is only one return for one value
+//although multiple return statements can be made
+//idealy there should be one return for one value
+//return statements end function execution so dont place in the middle
+
+//for example
+
+//function square(x) {
+//    return x * x;
+    //anything after this return will not run
+    //it will finish exection of this function
+//}
+
+function isPurple(color) {
+    if (color === 'purple') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
