30542b29a57d53c48c96383686b4ed97deb73de2
Randell Womack <randelljw92@gmail.com>
2021-03-30 14:27:28 -0700

    getcard() function example

--- app25.js
+++ app25.js
@@ -4,11 +4,26 @@
 
 //write a function called isPangram
 
-function isPangram(sentence) {
-    for (let char of 'abcdefghijklmnopqrstuvwxyz') {
-        if (sentence.indexOf (char) === -1) {
-            return false;
-        } 
-    }
-    return true;
+//function isPangram(sentence) {
+//    for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+//        if (sentence.indexOf (char) === -1) {
+//            return false;
+//        } 
+//    }
+//    return true;
+//}
+
+//function getcard() example
+
+function getCard() {
+    const values = [
+        '2', '3', '4', '5', '6', '7',
+        '8', '9', '10', 'j', 'q', 'k', 'A'
+    ];
+    const valIdx = Math.floor(Math.random() * values.length);
+    const value = values[valIdx];
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+    const suitIdx = Math.floor(Math.random() * suits.length);
+    const suit = suits[suitIdx];
+    return { value: value, suit: suit };
 }
