554264e6fc7ba6fceb0e8ca943d1e59e2a633ea7
Randell Womack <randelljw92@gmail.com>
2021-03-30 14:44:12 -0700

    function pick() to getCard()

--- app25.js
+++ app25.js
@@ -15,15 +15,34 @@
 
 //function getcard() example
 
+//function getCard() {
+//    const values = [
+//        '2', '3', '4', '5', '6', '7',
+//        '8', '9', '10', 'j', 'q', 'k', 'A'
+//    ];
+//    const valIdx = Math.floor(Math.random() * values.length);
+//    const value = values[valIdx];
+//    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+//    const suitIdx = Math.floor(Math.random() * suits.length);
+//    const suit = suits[suitIdx];
+//    return { value: value, suit: suit };
+//}
+
+//shorter example
+//function pick to recall the same code
+
+function pick(arr) {
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+
 function getCard() {
     const values = [
         '2', '3', '4', '5', '6', '7',
         '8', '9', '10', 'j', 'q', 'k', 'A'
     ];
-    const valIdx = Math.floor(Math.random() * values.length);
-    const value = values[valIdx];
+   const value = pick(values);
     const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
-    const suitIdx = Math.floor(Math.random() * suits.length);
-    const suit = suits[suitIdx];
+    const suit = pick(suits);
     return { value: value, suit: suit };
 }
