a3ea9637188f401eb33481e65ae3fbfa54dd28d5
Randell Womack <randelljw92@gmail.com>
2021-03-30 13:37:51 -0700

    developing a function to find average value in an array of numbers

--- app24.js
+++ app24.js
@@ -26,9 +26,24 @@
 //}
 //another example
 
-function isValidPassword(password, username) {
-    const tooShort = password.length <= 8;
-    const hasSpace = password.indexOf(' ') !== -1;
-    const tooSimilar = password.indexOf(username) !== -1;
-    return !tooShort && !hasSpace && !tooSimilar;
+//function isValidPassword(password, username) {
+//    const tooShort = password.length <= 8;
+//    const hasSpace = password.indexOf(' ') !== -1;
+//    const tooSimilar = password.indexOf(username) !== -1;
+//    return !tooShort && !hasSpace && !tooSimilar;
+//}
+
+//writing a function to find the average value in an array of numbers
+//avg([0,50]) //25
+//avg([75,76,80,95,100]) //85.2
+//format example
+
+
+function avg(arr) {
+    let total = 0;
+    arr.forEach(x => total = total + x);
+    console.log(total / arr.length);
 }
+avg([0, 50])
+avg([75, 76, 80, 95, 100])
+
