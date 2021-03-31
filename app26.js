ff329efbcc1cf761feb0e70d3ebdd36238a33490
Randell Womack <randelljw92@gmail.com>
2021-03-31 12:37:17 -0700

    another example of function scope

--- app26.js
+++ app26.js
@@ -1,20 +1,30 @@
 //function scope
 
-function lol() {
-    let person = 'Tom';
-    const age = 45;
-    var color = 'Teal';
-    console.log(age)
-}
+//function lol() {
+//   let person = 'Tom';
+//    const age = 45;
+//    var color = 'Teal';
+//    console.log(age)
+//}
 //lol();
 //console.log(person);
 //you get an error because 'person' is within the scope
 //of the specific function
 
-function changeColor() {
-    let color = 'purple';
-    const age = 19;
-    console.log(age);
+//function changeColor() {
+//    let color = 'purple';
+//    const age = 19;
+//    console.log(age);
+//}
+//lol();
+//changeColor();
+
+let bird = 'mandarin duck'
+
+function birdWatch() {
+    let bird = 'golden pheasant'
+    console.log(bird);
+
 }
-lol();
-changeColor();
+birdWatch();
+console.log(bird);
