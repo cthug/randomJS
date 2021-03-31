89828817c62d71fb981b0c67328277de984247da
Randell Womack <randelljw92@gmail.com>
2021-03-31 13:17:47 -0700

    block scope understanding

--- app26.js
+++ app26.js
@@ -19,12 +19,57 @@
 //lol();
 //changeColor();
 
-let bird = 'mandarin duck'
+//let bird = 'mandarin duck'
 
-function birdWatch() {
-    let bird = 'golden pheasant'
-    console.log(bird);
+//function birdWatch() {
+//    let bird = 'golden pheasant'
+//    console.log(bird);
+//}
+//birdWatch();
+//console.log(bird);
 
+//block scope
+//if (true) {
+//    let animal = 'eel';
+//    console.log(animal); //will work
+//}
+////console.log(animal);
+ //but outside this block of code above it will not work
+
+//if (true) {
+//    const animal = 'eel';
+//    console.log(animal);//will work also
+//}
+//console.log(animal);
+//this wont work outside the block
+//if (true) {
+//    var animal = 'eel';
+//    console.log(animal); //will work
+//}
+//console.log(animal);
+//var works outside the block unlike const and let
+//this can create issues for example
+
+//let animals = ['bear', 'goat', 'horse', 'dog']
+//for (var i = 0; i < animals.length; i++){
+//    console.log(i, animals[i]);
+//}
+//console.log(i);
+//printing the console.log outside the block will print 3 because  the for loop
+//, i++)  even if i made a var variable outide the block
+//it would treat it differently
+//let and const are block scoped var is not
+
+//block scope 
+function doubleArr(arr) {
+    const result = [];
+    for (let num of arr) {
+        let double = num * 2;
+        result.push(double);
+    }
+    return result;
 }
-birdWatch();
-console.log(bird);
+//put any numbers into calling this function
+//for example
+//doubleArr([1, 2, 3])
+//let is within the for loop block scope
