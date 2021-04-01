be3b5f350f933829946f47224ed577219b6d7478
Randell Womack <randelljw92@gmail.com>
2021-03-31 17:14:46 -0700

    function expressions

--- app27.js
+++ app27.js
@@ -1,20 +1,46 @@
 //lexical scope
-function outer() {
-    let movie = 'Amadeus'
+//function outer() {
+//    let movie = 'Amadeus'
 
-    function inner() {
-        console.log(movie.toUpperCase())
+//   function inner() {
+ //       console.log(movie.toUpperCase())
     
-    function extraInner() {
-        let movie = 'the shining'
-        console.log(movie.toUpperCase());
-    }
-    extraInner();
-}
-    inner()
-}
-outer()
+ //   function extraInner() {
+//        let movie = 'the shining'
+//        console.log(movie.toUpperCase());
+//    }
+//    extraInner();
+//}
+///    inner()
+//}
+//outer()
 
 //lexical scope simply is a nested function within a function
 //and the relationship between the parent function and inner function
 //the inner is always bound to outer function
+
+//now function expressions
+
+function add(x, y) {
+    return x + y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+const subtract = function (x, y) {
+    return x - y;
+}
+const divide = function (x, y) {
+    return x / y;
+}
+
+
+const operations = [add, multiply, subtract, divide];
+
+for (let func of operations) {
+    let result = func(30, 5);
+    console.log(result);
+}
+const thing = {
+    doSomething: multiply
+}
