02395e71c739c2e82bbac747bbf31105ef40af13
Randell Womack <randelljw92@gmail.com>
2021-04-02 13:45:34 -0700

    simple anon function use

--- app28.js
+++ app28.js
@@ -1,12 +1,28 @@
 //callback functions
 
-function callTwice(func) {
-    func();
-    func();
-}
-function laugh() {
-    console.log('haha!');
-}
-callTwice(laugh)
+//function callTwice(func) {
+//    func();
+//    func();
+//}
+//function laugh() {
+//    console.log('haha!');
+//}
+//callTwice(laugh)
+
 //this calls the function callTwice,and calls laugh its an example
 //of callback function
+
+//sometimes anonymous function is needed
+
+//setTimeout(function () {
+//    alert('Welcome!')
+//}, 2000);
+
+
+//function alpha() {
+//    alert('Hello Tatum i knew you would click this button...self destruct in 10 seconds')
+//}
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+    alert('Tatum the button says dont click me! ')
+})
