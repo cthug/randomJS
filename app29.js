055d4881a01ebac57619023674ce9493798ecb9e
Randell Womack <randelljw92@gmail.com>
2021-04-02 16:46:59 -0700

    more array call back

--- app29.js
+++ app29.js
@@ -1,6 +1,6 @@
 //Hoisting
-var animal = 'Tapir';
-console.log(animal);
+//var animal = 'Tapir';
+//console.log(animal);
 //hoisting happens when var is declared after
 //var will always act first but will give msg of undefined
 //for example
@@ -19,45 +19,83 @@ console.log(animal);
 //answer();
 //speak();
 
-function call() {
-    console.log(`homeowner:ring..ring..ring`);
-}
 
-function answer() {
-    console.log(`dispatch:hello? 911`)
-}
-function speak() {
-    console.log(`homeowner:uhh yes theres a tapir here in my yard...`)
-}
-function chill() {
-    console.log(`dispatch:wow! okay sir? ill get a car out right away!`)
-}
+//function call() {
+//    console.log(`homeowner:ring..ring..ring`);
+//}
+//
+//function answer() {
+//    console.log(`dispatch:hello? 911`)
+//}
+//function speak() {
+//    console.log(`homeowner:uhh yes theres a tapir here in my yard...`)
+//}
+//function chill() {
+//    console.log(`dispatch:wow! okay sir? ill get a car out right away!`)
+//}
+//
+//function no() {
+//    console.log(`homeowner:no sir no cops at all needed, i just need legal advice...`)
+//}
+//function ok() {
+//    console.log(`dispatch:uuhhhh`)
+//}
+//function bombshell() {
+//    console.log(`homeowner:yeah hold your mouth shut i want to double check on the legal aspect of a tapir kickin kegger?`)
+//}
+////bombshell();
+////well();
+////yes();
+////nice();
+//
+//function well() {
+//    console.log(`dispatch:well sir..`)
+//}
+//function yes() {
+//    console.log(`homeowner:no laws?Thats what i thought so listen son keep it on the hush but your invited to a tapir kickin kegger`)    
+//}
+//function nice() {
+//    console.log(`dispatch:okay sir...ill be there sounds fun.`)
+//}
+//
+//function total() {
+//        return (call(), answer(), speak(),chill(), bombshell(), well(), yes(), nice());
+//}
+//total();
 
-function no() {
-    console.log(`homeowner:no sir no cops at all needed, i just need legal advice...`)
-}
-function ok() {
-    console.log(`dispatch:uuhhhh`)
-}
-function bombshell() {
-    console.log(`homeowner:yeah hold your mouth shut i want to double check on the legal aspect of a tapir kickin kegger?`)
-}
-//bombshell();
-//well();
-//yes();
-//nice();
+//array callback method
+//const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+//numbers.forEach(function (num) {
+//    console.log(num * 2);
+//})
 
-function well() {
-    console.log(`dispatch:well sir..`)
-}
-function yes() {
-    console.log(`homeowner:no laws?Thats what i thought so listen son keep it on the hush but your invited to a tapir kickin kegger`)    
-}
-function nice() {
-    console.log(`dispatch:okay sir...ill be there sounds fun.`)
-}
+//function printTriple(n) {
+//    console.log(n * 3);
+//}
+//numbers.forEach(printTriple);
 
-function total() {
-        return (call(), answer(), speak(),chill(), bombshell(), well(), yes(), nice());
-}
-total();
+//a more complex array where each element is an object  
+const books = [{
+    title: 'Good Omens',
+    authors:['Terry Pratchett, Neil Gaiman'],
+    rating: 4.25
+},
+{  
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42
+},
+{
+        title:'American Gods',
+        authors:['Neil Gaiman'],
+        rating:4.11
+},
+{
+        title:'A Gentleman In Moscow',
+        authors:['Amor Towles'],
+        rating:4.36
+    }
+]
+books.forEach(function (book) {
+    console.log(book.title.toUpperCase())
+})
