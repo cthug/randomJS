db570fc3c6ca9d6778669542542773c10d64a91f
Randell Womack <randelljw92@gmail.com>
2021-04-03 09:36:57 -0700

    using map

--- app29.js
+++ app29.js
@@ -75,27 +75,48 @@
 //numbers.forEach(printTriple);
 
 //a more complex array where each element is an object  
-const books = [{
-    title: 'Good Omens',
-    authors:['Terry Pratchett, Neil Gaiman'],
-    rating: 4.25
-},
-{  
-    title: 'Bone: The Complete Edition',
-    authors: ['Jeff Smith'],
-    rating: 4.42
-},
-{
-        title:'American Gods',
-        authors:['Neil Gaiman'],
-        rating:4.11
-},
-{
-        title:'A Gentleman In Moscow',
-        authors:['Amor Towles'],
-        rating:4.36
+//const books = [{
+//    title: 'Good Omens',
+//    authors:['Terry Pratchett, Neil Gaiman'],
+//    rating: 4.25
+//},
+//{  
+//    title: 'Bone: The Complete Edition',
+//    authors: ['Jeff Smith'],
+//    rating: 4.42
+//},
+//{
+//        title:'American Gods',
+//        authors:['Neil Gaiman'],
+//        rating:4.11
+//},
+//{
+//        title:'A Gentleman In Moscow',
+//        authors:['Amor Towles'],
+//        rating:4.36
+//    }
+//]
+//books.forEach(function (book) {
+//    console.log(book.title.toUpperCase())
+//}
+ 
+
+//Map
+
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+const words = ['asap', 'byob', 'rsvp', 'diy'];
+ 
+const doubles = numbers.map(function (num) {
+    return num * 2;
+});
+
+const numDetail = numbers.map(function (n) {
+    return {
+        Value: n,
+        isEven: n % 2 === 0
     }
-]
-books.forEach(function (book) {
-    console.log(book.title.toUpperCase())
+})
+
+const abrevs = words.map(function (word) {
+    return word.toUpperCase().split('').join('.');
 })
