977a77a0d87f2876ce20b293261f6e6e90b98231
Randell Womack <randelljw92@gmail.com>
2021-04-05 16:07:26 -0700

    array find

--- app30.js
+++ app30.js
@@ -30,21 +30,36 @@
  
 //const square = n => n * n;
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+//const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+//
+//const doubles1 = nums.map(function (n){
+//    return n * 2;
+//})
+//
+//const doubles2 = nums.map(n => {
+//    return n * 2;
+//})
+//
+//const doubles3 = nums.map(n => n * 2);
+//
+//const parityList = nums.map(function (n) {
+//    if (n % 2 === 0) return "even";
+//    return "odd";
+//});
+//const parityList = nums.map((n) => n % 2 === 0 ? 'even' : 'odd');
 
-const doubles1 = nums.map(function (n){
-    return n * 2;
-})
+ //array find
 
-const doubles2 = nums.map(n => {
-    return n * 2;
-})
-
-const doubles3 = nums.map(n => n * 2);
-
-const parityList = nums.map(function (n) {
-    if (n % 2 === 0) return "even";
-    return "odd";
-});
-const parity = nums.map((n) => n % 2 === 0 ? 'even' : 'odd');
-
+let movies = [
+    "The Fantastic four",
+    "Mr. and Mrs. Smith",
+    "Mrs. Doubtfire",
+    "Mr. Deeds"
+]
+ const movie = movies.find(movie => {
+    return movie.includes('Mrs');
+ })
+const movie2 = movies.find(m => (
+    m.indexOf("Mrs") === 0
+))
+ 
