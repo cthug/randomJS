c8bef50200f4026dc506803cf8a7423a5a5ff5e9
Randell Womack <randelljw92@gmail.com>
2021-04-05 15:23:06 -0700

    arrow function & implicit return

--- app29.js
+++ app29.js
@@ -103,20 +103,21 @@
 
 //Map
 
-const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
-const words = ['asap', 'byob', 'rsvp', 'diy'];
- 
-const doubles = numbers.map(function (num) {
-    return num * 2;
-});
-
-const numDetail = numbers.map(function (n) {
-    return {
-        Value: n,
-        isEven: n % 2 === 0
-    }
-})
-
-const abrevs = words.map(function (word) {
-    return word.toUpperCase().split('').join('.');
-})
+//const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+//const words = ['asap', 'byob', 'rsvp', 'diy'];
+// 
+//const doubles = numbers.map(function (num) {
+//    return num * 2;
+//});
+//
+//const numDetail = numbers.map(function (n) {
+//    return {
+//        Value: n,
+//        isEven: n % 2 === 0
+//    }
+//})
+//
+//const abrevs = words.map(function (word) {
+//    return word.toUpperCase().split('').join('.');
+//})
+//
---
+++ app30.js
@@ -0,0 +1,31 @@
+//arrow functions
+
+// const square = function(x){
+//return x * x;
+//}
+//examples
+//const square = (x) => {
+//    return x * x;
+//}
+//
+//const isEven = (num)  => {
+//    return num % 2 === 0;
+//}
+//const multiply = (x, y) => {
+//    return x * y;
+//}
+//const greet = () => {
+//    console.log("Hi!")
+// }
+
+//implicit return
+
+//const square = (n) => {
+//    return n * n;
+//}
+
+//const square = n => (
+//    n * n
+//);
+const square = n => n * n;
+
