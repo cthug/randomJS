3dbb7b9b9396787272e66029c1813c6a3a6e23fe
Randell Womack <randelljw92@gmail.com>
2021-04-10 11:22:46 -0700

    simple example using this in methods

--- This38.js
+++ This38.js
@@ -4,8 +4,45 @@ function sayHI() {
     console.log(this);
 }
 
-//'this' inside a function refers to the window
+//'this' refers to the window global scope object in the browser in example above
 
-const greet = function () {
-    console.log(this);
+//const greet = function () {
+//    console.log(this);
+//}
+
+//const person = {
+//    first: 'joey',
+//    last: 'donuts',
+//    nickName: 'Joey bag of donuts',
+//    fullName() {
+//        console.log(this);
+//    }
+//}
+//the value of 'this' in above example
+//has been set to the object this method is inside.
+
+//'this' can be used inside the object like this example as below
+//const person = {
+//    first: 'joey',
+//    last: 'donuts',
+//    nickName: 'Joey bag of donuts',
+//    fullName() {
+//        console.log(`${this.first} ${this.last} AKA ${this.nickName}`)
+//    }
+//}
+
+//or we could write this way using destructuring
+const person = {
+    first: 'joey',
+    last: 'donuts',
+    nickName: 'Joey bag of donuts',
+    fullName() {
+        const {
+            first,
+            last,
+            nickName
+        } = this;
+        console.log(`${first} ${last} AKA ${nickName}`)
+    }
 }
+
